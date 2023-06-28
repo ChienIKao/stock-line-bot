7d2db83bcaec56fea66d47efb978708a91e9c87e
ChienI Kao <alen911018@gmail.com>
2023-06-28 15:40:53 +0800

    first commit

--- api/index.py
+++ api/index.py
@@ -11,15 +11,19 @@ line_bot_api = LineBotApi(os.getenv("LINE_CHANNEL_ACCESS_TOKEN"))
 line_handler = WebhookHandler(os.getenv("LINE_CHANNEL_SECRET"))
 working_status = os.getenv("DEFALUT_TALKING", default = "true").lower() == "true"
 
+# domain root
 @app.route('/')
 def home():
-    return 'Hello'
+    return 'Hello, World!'
 
-@app.route('/webhook', methods=['POST'])
+@app.route("/webhook", methods=['POST'])
 def callback():
+    # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
+    # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
+    # handle webhook body
     try:
         line_handler.handle(body, signature)
     except InvalidSignatureError:

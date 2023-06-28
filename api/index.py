0bcfecef1cf47aff2a4eddd1ae2fc97810e12a11
ChienI Kao <alen911018@gmail.com>
2023-06-28 15:45:54 +0800

    first commit

--- api/index.py
+++ api/index.py
@@ -2,6 +2,7 @@ from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from api.chatgpt import ChatGPT
 
 import os
 
@@ -10,6 +11,7 @@ line_handler = WebhookHandler(os.getenv("LINE_CHANNEL_SECRET"))
 working_status = os.getenv("DEFALUT_TALKING", default = "true").lower() == "true"
 
 app = Flask(__name__)
+chatgpt = ChatGPT()
 
 # domain root
 @app.route('/')
@@ -30,10 +32,35 @@ def callback():
         abort(400)
     return 'OK'
 
+
 @line_handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
-    line_bot_api.reply_message(event.reply_token, message)
+    global working_status
+    if event.message.type != "text":
+        return
+
+    if event.message.text == "說話":
+        working_status = True
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text="我可以說話囉，歡迎來跟我互動 ^_^ "))
+        return
+
+    if event.message.text == "閉嘴":
+        working_status = False
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text="好的，我乖乖閉嘴 > <，如果想要我繼續說話，請跟我說 「說話」 > <"))
+        return
+
+    if working_status:
+        chatgpt.add_msg(f"HUMAN:{event.message.text}?\n")
+        reply_msg = chatgpt.get_response().replace("AI:", "", 1)
+        chatgpt.add_msg(f"AI:{reply_msg}\n")
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=reply_msg))
+
 
 if __name__ == "__main__":
     app.run()

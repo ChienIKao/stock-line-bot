9729f56b0729eaed3b9823e723c9ec2f057df335
ChienI Kao <alen911018@gmail.com>
2023-06-28 15:38:24 +0800

    first commit

--- api/index.py
+++ api/index.py
@@ -9,6 +9,7 @@ app = Flask(__name__)
 
 line_bot_api = LineBotApi(os.getenv("LINE_CHANNEL_ACCESS_TOKEN"))
 line_handler = WebhookHandler(os.getenv("LINE_CHANNEL_SECRET"))
+working_status = os.getenv("DEFALUT_TALKING", default = "true").lower() == "true"
 
 @app.route('/')
 def home():

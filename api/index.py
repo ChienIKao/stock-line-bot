0dc5691708c30cb88be9f6f99fbf55e9f79999e1
ChienI Kao <alen911018@gmail.com>
2023-06-28 19:33:21 +0800

    first commit

--- api/index.py
+++ api/index.py
@@ -44,14 +44,14 @@ def handle_message(event):
     if msg.text == "選股":
         reply = TYPE
     elif msg.text == "ETF" or msg.text == "1" or msg.text == "1.":
-        reply = "1. 00878.TW  : 國泰永續高股息" + \
-                "2. 00919.TW  : 群益台灣精選高息" + \
-                "3. 00713.TW  : 元大台灣高息低波" + \
-                "4. 0056.TW   : 元大高股息" + \
-                "5. 006208.TW : 富邦台灣50" + \
-                "6. 00690.TW  : 兆豐藍籌30" + \
-                "7. 00850.TW  : 元大臺灣ESG永續" + \
-                "8. 00701.TW  : 國泰股利精選30"
+        reply = "00878.TW  : 國泰永續高股息" + \
+                "00919.TW  : 群益台灣精選高息" + \
+                "00713.TW  : 元大台灣高息低波" + \
+                "0056.TW   : 元大高股息" + \
+                "006208.TW : 富邦台灣50" + \
+                "00690.TW  : 兆豐藍籌30" + \
+                "00850.TW  : 元大臺灣ESG永續" + \
+                "00701.TW  : 國泰股利精選30"
     elif msg.text == "績優股" or msg.text == "2" or msg.text == "2.":
         reply = "績優股"
     elif msg.text == "金融股" or msg.text == "3" or msg.text == "3.":
